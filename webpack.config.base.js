7f164bc17bf0df3e5facf538175faa79f607a14e
Ting <1353008829@qq.com>
2022-09-17 20:26:30 +0800

    update

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -11,8 +11,24 @@ module.exports = {
     title: 'My App',
     template: 'src/assets/index.html'
   }),
-],
+  ],
   module: {
-    rules: [],
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // 将 JS 字符串生成为 style 节点
+          'style-loader',
+          // 将 CSS 转化成 CommonJS 模块
+          'css-loader',
+          // 将 Sass 编译成 CSS
+          'sass-loader',
+        //   {
+        //     loader: 'sass-loader',
+        //     options: {sass: require('dart-sass')}
+        //   }
+        ],
+      },
+    ],
   },
 };
